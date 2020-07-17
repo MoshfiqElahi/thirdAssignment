90eeb627653ad3317eb2f9aacf6c7a2b77652d47
MoshfiqElahi <contact.moshfiq@gmail.com>
2020-07-17 20:00:32 +0600

    first, second and forth completed

--- assignment.js
+++ assignment.js
@@ -1,12 +1,27 @@
 function feetToMile(feet) {
-    var mile = feet/5280;
+    var mile = feet / 5280;
     return mile;
 }
 
-function woodCalculator(chair, table, bed){
+
+function woodCalculator(chair, table, bed) {
     var chairWood = chair * 1;
     var tableWood = table * 3;
     var bedWood = bed * 5;
     var totalWood = chairWood + tableWood + bedWood;
     return totalWood;
 }
+
+
+function tinyfriend(friend){
+    var smallName = Infinity;
+    var tinyName;
+    for (var i = 0; i < friend.length; i++) {
+        if (friend[i].length < smallName){
+            smallName = friend[i].length;
+            tinyName = friend[i];
+        }
+    }
+    return tinyName;
+}
+
