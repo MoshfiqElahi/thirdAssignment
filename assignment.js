ca77d593bab131368a1c17571b65a14e0f47de2f
MoshfiqElahi <contact.moshfiq@gmail.com>
2020-07-17 21:02:26 +0600

    assingment is maybe ready, waiting for last check

--- assignment.js
+++ assignment.js
@@ -13,11 +13,11 @@ function woodCalculator(chair, table, bed) {
 }
 
 
-function tinyfriend(friend){
+function tinyfriend(friend) {
     var smallName = Infinity;
     var tinyName;
     for (var i = 0; i < friend.length; i++) {
-        if (friend[i].length < smallName){
+        if (friend[i].length < smallName) {
             smallName = friend[i].length;
             tinyName = friend[i];
         }
@@ -25,3 +25,28 @@ function tinyfriend(friend){
     return tinyName;
 }
 
+function brickCalculator(building) {
+    var feetBricks = 1000;
+    if (building >= 0 && building <= 10) {
+        var totalFeet = (building * 15);
+        var totalBricks = (totalFeet + feetBricks);
+        return totalBricks;
+    }
+    else if (building >= 0 && building <= 20) {
+        var firstTenFloorBricks = 30000;
+        var totalFeet = (building * 12);
+        var totalBricks = (totalFeet * feetBricks) + firstTenFloorBricks;
+        return totalBricks;
+    }
+    else if (building >= 0 && building >= 21) {
+        var firstTenFloorBricks = 50000;
+        var secondFloorBricks = 20000;
+        var extraTotalBricks = (firstTenFloorBricks + secondFloorBricks);
+        totalFeet = (building * 10);
+        var totalBricks = (totalFeet * feetBricks) + extraTotalBricks;
+        return totalBricks;
+    }
+    else {
+        return "Input a positive value"
+    }
+}
