c57bfb2c43f8bf6b7b1d2ac83692e3e58b47813d
MoshfiqElahi <contact.moshfiq@gmail.com>
2020-07-17 09:02:49 +0600

    woodCalculator completed

--- assignment.js
+++ assignment.js
@@ -2,8 +2,6 @@ function feetToMile(feet) {
     var mile = feet/5280;
     return mile;
 }
-var mileResult = feetToMile(500052)
-console.log(mileResult)
 
 function woodCalculator(chair, table, bed){
     var chairWood = chair * 1;
@@ -11,10 +9,4 @@ function woodCalculator(chair, table, bed){
     var bedWood = bed * 5;
     var totalWood = chairWood + tableWood + bedWood;
     return totalWood;
-}
-var woodCount = woodCalculator(5, 5, 2)
-console.log(woodCount)
-
-function tinyFriend(name){
-    var tiny = name[0];
 }
